0876b96f58b30e63a99977c700c8585dd21b2933
Aravind <aravindsprint@gmail.com>
2025-09-18 22:54:56 +0530

    fix: move frappe import inside function to avoid installation issues

--- senbagam_api/__init__.py
+++ senbagam_api/__init__.py
@@ -1,5 +1,5 @@
-import frappe
 __version__ = '0.0.1'
 
 def after_install():
-	frappe.db.set_value("System Settings", "System Settings", "allow_login_using_mobile_number", 1)
+    import frappe
+    frappe.db.set_value("System Settings", "System Settings", "allow_login_using_mobile_number", 1)
